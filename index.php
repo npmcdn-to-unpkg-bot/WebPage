1642a8fc2015e2973627e60f3f15c6068810c694
mali1488 <mattias.linder87@gmail.com>
2016-03-09 13:45:18 +0100

    init

---
+++ index.php
@@ -0,0 +1,33 @@
+<html lang="en" class="no-js">
+<head>
+  <!-- Include css stylesheets and some other options -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/angular-material.min.css"></link>
+  <link rel="stylesheet" href="css/app.css">
+  
+  <title>wave-gotik-treffen</title>
+
+</head>
+<!--<body ng-app="flogstaEngine">-->
+<body> Hello Flogsta engine...
+
+<!-- jQuery imports-->
+<script type="js/deps/jQuery/1.12.1/jquery.js"></script>
+
+<!-- Angular imports -->
+<script src="js/deps/angular/1.5.0/angular.min.js"></script>
+<script src="js/deps/angular/1.5.0/angular-route.min.js"></script>
+<script src="js/deps/angular/1.5.0/angular-resource.min.js"></script>
+<script src="js/deps/angular/1.5.0/angular-animate.min.js"></script>
+<script src="js/deps/angular/1.5.0/angular-aria.min.js"></script>
+<script src="js/deps/angular/1.5.0/angular-messages.min.js"></script>
+<script src="js/deps/angular/angular-material/1.0.0/angular-material.min.js"></script>
+
+<!-- Other imports -->
+
+<!-- Local imports -->
+
+</body>
+</html>

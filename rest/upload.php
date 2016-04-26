ef945c5993587be23cccf8babc81f0370ecca72a
Simon Bergström <simon@menuan.se>
2016-04-26 19:41:22 +0200

    A sneaky png file disguised itself as a jpeg and killed my PHP script.

--- rest/upload.php
+++ rest/upload.php
@@ -12,39 +12,42 @@ function createInsertQuery($hash_id) {
 
 function handleUploadedFiles() {
   $emptyOrNotEmpty = !empty($_FILES['images']);
-  print 'sizeof($_FILES)\n';
-  $x = sizeof($_FILES['images']['name']);
-  print $x;
   $result = array();
   if ($emptyOrNotEmpty) {
     $db = new DBCon();
     $db->connect();
     $curDate = date("Y/m/d");
-    print("We have received $x images!\n");
     foreach ($_FILES['images']['name'] as $key => $fileName) {
-      $hash_id = hash_file("sha256", $_FILES['images']['tmp_name'][$key]);
+      $fileResult = array('id' => $_FILES['images']['name'][$key]);              // Preparing a logging variable for any errors
+      $hash_id = hash_file("sha256", $_FILES['images']['tmp_name'][$key]);       // that might occur.
+      
       $sqlCheck = "SELECT COUNT(*) AS count FROM photos WHERE id = '$hash_id';"; // Query to check if image already exists
       $extTmp = explode(".", $_FILES['images']['name'][$key]);
       $extension = $extTmp[count($extTmp) - 1];
       $destinationPath = generate_path($curDate, $hash_id);
       $fullFilePath = $destinationPath . "/original." . $extension;
+
       if ($db->countQuery($sqlCheck) == "1") {                                   // Check if already exists in database
         print "MySQL entry already exists, skipping MySQL entry...\n";           // Abort handling this entry
-        array_push($result, array('error', 'file_exists_in_db'));
+        array_push($fileResult['errors'], 'file_exists_in_db');          // Push an error saying file exists in db
       } else {
         $sqlInsert = createInsertQuery($hash_id);
         $check = $db->insert_query($sqlInsert);
       }
+
       if (file_exists($fullFilePath)) {                                          // Check whether the file exists in the filesystem
         print "File exists in filesystem, skipping...\n";                        // already, and skip moving if so.
-        array_push($result, array('error', 'file_exists_in_fs'));
+        array_push($fileResult, array('error' => 'file_exists_in_fs'));          // Push an error saying file exists in fs
       } else{
         move_uploaded_file($_FILES['images']['tmp_name'][$key], $fullFilePath);
         generate_imgs($fullFilePath, $destinationPath, $extension);
       }
+      if (!empty($fileResult))
+        array_push($result, $fileResult);
     }
+    
     if (empty($result))
-      return "{status, ok}";
+      return array('status' => 'ok');
     return $result;
   }
 }

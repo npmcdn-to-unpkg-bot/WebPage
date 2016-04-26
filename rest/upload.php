703e323bb87a90d7645dda886344f8eda9428c64
Simon Bergström <simon@menuan.se>
2016-04-26 20:14:46 +0200

    This will have to do for now. Pushing and will merge with Mattias' latest code.

--- rest/upload.php
+++ rest/upload.php
@@ -18,7 +18,8 @@ function handleUploadedFiles() {
     $db->connect();
     $curDate = date("Y/m/d");
     foreach ($_FILES['images']['name'] as $key => $fileName) {
-      $fileResult = array('id' => $_FILES['images']['name'][$key]);              // Preparing a logging variable for any errors
+      $fileResult = array('id' => $_FILES['images']['name'][$key],               // Preparing a logging variable for any errors
+                          'errors' => array());
       $hash_id = hash_file("sha256", $_FILES['images']['tmp_name'][$key]);       // that might occur.
       
       $sqlCheck = "SELECT COUNT(*) AS count FROM photos WHERE id = '$hash_id';"; // Query to check if image already exists
@@ -27,23 +28,22 @@ function handleUploadedFiles() {
       $destinationPath = generate_path($curDate, $hash_id);
       $fullFilePath = $destinationPath . "/original." . $extension;
 
-      if ($db->countQuery($sqlCheck) == "1") {                                   // Check if already exists in database
-        print "MySQL entry already exists, skipping MySQL entry...\n";           // Abort handling this entry
-        array_push($fileResult['errors'], 'file_exists_in_db');          // Push an error saying file exists in db
+      if ($db->countQuery($sqlCheck) == "1") {                                   // Check if the file already exists in database
+        array_push($fileResult['errors'], 'file_exists_in_db');                  // Push an error saying file exists in db
       } else {
         $sqlInsert = createInsertQuery($hash_id);
         $check = $db->insert_query($sqlInsert);
       }
 
       if (file_exists($fullFilePath)) {                                          // Check whether the file exists in the filesystem
-        print "File exists in filesystem, skipping...\n";                        // already, and skip moving if so.
-        array_push($fileResult, array('error' => 'file_exists_in_fs'));          // Push an error saying file exists in fs
+        array_push($fileResult['errors'], 'file_exists_in_fs');                  // Push an error saying file exists in fs
       } else{
         move_uploaded_file($_FILES['images']['tmp_name'][$key], $fullFilePath);
         generate_imgs($fullFilePath, $destinationPath, $extension);
       }
-      if (!empty($fileResult))
-        array_push($result, $fileResult);
+      if (empty($fileResult['errors']))
+        $fileResult['status'] = 'ok';
+      array_push($result, $fileResult);
     }
     
     if (empty($result))
